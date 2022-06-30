daca02569a9ad6346dcceb525d9a8472866f53a1
Fan <yfctw555@gmail.com>
2022-06-30 16:29:31 +0800

    增加後台主題列表欄位，加css美化

--- admin.php
+++ admin.php
@@ -11,8 +11,8 @@ include_once "./api/base.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>投票管理中心</title>
-    <link rel="stylesheet" href="./css/basic.css">
-    <link rel="stylesheet" href="./css//back.css">
+    <link rel="stylesheet" href="./css//basic.css">
+    <link rel="stylesheet" href="./css/back.css">
 </head>
 
 <body>
@@ -35,13 +35,43 @@ include_once "./api/base.php";
             <button class="btn btn-primary" onclick="location.href='?do=add_vote'">新增投票</button>
             <div>
                 <ul>
+                    <li class="list-header">
+                        <div>投票主題</div>
+                        <div>單/複選</div>
+                        <div>投票期間</div>
+                        <div>剩餘天數</div>
+                        <div>投票人數</div>
+                        <div>功能</div>
+                    </li>
                     <?php
                     $subjects = all('subjects');
                     foreach ($subjects as $subject) {
                         echo "<li class='list-items'>";
-                        echo $subject['subject'];
+                        echo "<div>{$subject['subject']}</div>";
+                        if($subject['multiple']==0){
+                            echo "<div class='text-center'>單選</div>";
+                        }else{
+                            echo "<div class='text-center'>複選</div>";
+                        }
+                        echo "<div class='text-center'>";
+                        echo $subject['start']." ~ ".$subject['end'];
+                        echo "</div>";
+
+                        echo "<div class='text-center'>";
+                             $today=strtotime("now");
+                             $end=strtotime($subject['end']);
+                             if(($end-$today)>0){
+                                $remain=floor(($end-$today)/(60*60*24));
+                                echo "投票倒數".$remain."天結束";
+                             }else{
+                                echo "<span style='color:grey'>投票結束</span>";
+                             }
+                        echo "</div>";
+                        echo "<div class='text-center'>{$subject['total']}</div>";
+                        echo "<div class='text-center'>";
                         echo "<a class='edit' href='?do=edit&id={$subject['id']}'>編輯</a>";
                         echo "<a class='dele' href='?do=dele&id={$subject['id']}'>刪除</a>";
+                        echo "</div>";
                         echo "</li>";
                     }
                     ?>
